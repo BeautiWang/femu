c0f87c7e73b0fe3fb348a644682b5b2aed256cff
wangke-25 <296574397@qq.com>
2020-12-30 15:00:01 +0800

    try

--- hw/block/femu/ftl/ftl.c
+++ hw/block/femu/ftl/ftl.c
@@ -292,7 +292,8 @@ static void ssd_init_params(struct ssdparams *spp)
     spp->secs_per_pg = 8;
     spp->pgs_per_blk = 256;
     // spp->blks_per_pl = 256; /* 16GB */
-    spp->blks_per_pl = 80; /* 5GB */
+    // spp->blks_per_pl = 80; /* 5GB */
+    spp->blks_per_pl = 160; /* 10GB */
     spp->pls_per_lun = 1;
     spp->luns_per_ch = 8;
     spp->nchs = 8;
